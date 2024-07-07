e37bc46e4f082106070594f22c8978b7c54110c3
Sarah Mc Loughlin <boosarahs@gmail.com>
2024-07-07 12:35:44 +0000

    Add libraries, dependencies, colorama

--- run.py
+++ run.py
@@ -3,6 +3,15 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
+# Import colorama modules, adapted from tutorial: https://linuxhint.com/colorama-python/
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+import sys
+import os
+import time
+
 # IAM Configuration
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -15,9 +24,29 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('print_statement') # use spreadsheet name here
 
-# Variables
-sales = SHEET.worksheet('sales')
+# Functions
+def clearScreen():
+    """
+    Clear CLI function
+    """
+    os.system("clear")
 
-data = sales.get_all_values()
+def typingPrint(text, color=Fore.WHITE):
+    """
+    Use typingPrint() in place of print() creates typing effect
+    """
+    for character in text:
+        sys.stdout.write(color + character)
+        sys.stdout.flush()
+        time.sleep(0.05)
 
-print(data)
+def welcome():
+    """
+    Display welcome screen text
+    """
+    typingPrint("Welcome to Print Statement\n",Fore.LIGHTMAGENTA_EX)
+    typingPrint("A sales revenue and inventory management system for sarah_prints_inc.")
+    time.sleep(5)
+    clearScreen()
+
+welcome()
