179ead5f98a8ea84c2911da412047919d47f67bf
Sarah Mc Loughlin <boosarahs@gmail.com>
2024-07-08 11:32:50 +0000

    Fix indentations and whitespace as per python linter testing

--- run.py
+++ run.py
@@ -2,15 +2,15 @@
 # Import dependencies
 import gspread
 from google.oauth2.service_account import Credentials
-
-# Import colorama modules, adapted from tutorial: https://linuxhint.com/colorama-python/
-import colorama
-from colorama import Fore, Back, Style
-colorama.init(autoreset=True)
-
 import sys
 import os
 import time
+
+# Import colorama modules, adapted from tutorial:
+# https://linuxhint.com/colorama-python/
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
 
 # IAM Configuration
 SCOPE = [
@@ -22,14 +22,17 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('print_statement') # use spreadsheet name here
+SHEET = GSPREAD_CLIENT.open('print_statement')  # use spreadsheet name here
 
 # Functions
+
+
 def clearScreen():
     """
     Clear CLI function
     """
     os.system("clear")
+
 
 def typingPrint(text, color=Fore.WHITE):
     """
@@ -40,15 +43,17 @@ def typingPrint(text, color=Fore.WHITE):
         sys.stdout.flush()
         time.sleep(0.05)
 
+
 def typeInput(text):
     for character in text:
         sys.stdout.write(character)
         sys.stdout.flush()
         time.sleep(0.05)
 
+
 def welcome():
     """
-    Display Print Statement Banner 
+    Display Print Statement Banner
     """
     print(Fore.MAGENTA + Style.BRIGHT + r'''
        $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
@@ -74,44 +79,51 @@ def welcome():
     """
     Display welcome screen text
     """
-    typingPrint("                      ** Welcome to Print Statement **\n",Fore.YELLOW + Style.BRIGHT                          )
+    typingPrint("                      ** Welcome to Print Statement **\n",
+                Fore.YELLOW + Style.BRIGHT)
     time.sleep(1)
-    typingPrint("     A sales and inventory management system for Sarah's Screen Prints Inc.",Fore.WHITE)
+    typingPrint("     A sales and inventory management system "
+                "for Sarah's Screen Prints Inc.", Fore.WHITE)
     print("\n")
     time.sleep(4.5)
     clearScreen()
 
     print("\n")
-    typingPrint(" Print Statement",Fore.YELLOW + Style.BRIGHT )
+    typingPrint(" Print Statement", Fore.YELLOW + Style.BRIGHT)
     typingPrint(" is a comprehensive inventory management system.\n")
     time.sleep(1)
-    typingPrint(" This program is for an artist's small screen-printing business.\n")
+    typingPrint(" This program is for an artist's small "
+                "screen-printing business.\n")
     time.sleep(1)
-    typingPrint(" It enables users to monitor sales, stock, orders and materials.\n")
+    typingPrint(" It enables users to monitor sales, "
+                "stock, orders and materials.\n")
     time.sleep(3)
+
 
 welcome()
 
-def options():
 
+def options():
     print("\n")
 
     print(Fore.YELLOW + Style.BRIGHT + " Please choose from the menu below:\n")
     time.sleep(2)
-    print ("   1. Market Sales\n"
-    "   2. Online Sales\n"
-    "   3. Print Stock\n"
-    "   4. Product Surplus\n"
-    "   5. Materials\n"
-    "   6. Exit Program\n")
+    print("   1. Market Sales\n"
+          "   2. Online Sales\n"
+          "   3. Print Stock\n"
+          "   4. Product Surplus\n"
+          "   5. Materials\n"
+          "   6. Exit Program\n")
 
     while True:
-        data_str = input(Fore.YELLOW + Style.BRIGHT + " Enter a number to access data here: \n")
+        data_str = input(Fore.YELLOW + Style.BRIGHT +
+                         " Enter a number to access data here: \n")
 
     """
     while True:
         try:
-            option = int(typeInput(" Choose a number to access corresponding data:\n"))
+            option = int(typeInput("
+            Choose a number to access corresponding data:\n"))
             if option == 1:
                 view_sales()
                 break
@@ -133,4 +145,6 @@ def options():
         except ValueError:
             print(Fore.RED + "Please choose a valid number")
     """
+
+
 options()
